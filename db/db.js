5dd1c0ec860cdf830ab5c1b467ddd62cad2ac614
Adam Slack <adamslack@outlook.com>
2017-10-09 09:46:36 +0100

    config and DB added

---
+++ db/db.js
@@ -0,0 +1,114 @@
+const config = require('../config/config.json');
+const pg = require('pg');
+
+class DB {
+    /**
+     *  DB Class constructor - Initialises all config for pg db connections
+     *  as well as a pool for connecting to xray db.
+     */
+    constructor(module) {
+        // WISHLIST: initialise pool for desired db option from the config here.
+        const dbCfg = config.db;
+        dbCfg.user = config[module].db.user;
+        dbCfg.password = config[module].db.password;
+        dbCfg.max = 10;
+        dbCfg.idleTimeoutMillis = 30000;
+
+        // this initializes a connection pool
+        // it will keep idle connections open for 30 seconds
+        // and set a limit of maximum 10 idle clients
+        this.pool = new pg.Pool(dbCfg);
+
+        this.pool.on('error', (err) => {
+            console.log('idle client error', err.message, err.stack);
+        });
+    }
+
+    // export the query method for passing queries to the pool
+    query(text, values) {
+        try {
+            if (values) console.log('query:', text, values);
+            else console.log('query:', text);
+            return this.pool.query(text, values);
+        } catch (err) {
+            console.log('Error With Postgres Query');
+            throw err;
+        }
+    }
+
+    async connect() {
+        try {
+            console.log('connecting to db pool');
+            const ret = await this.pool.connect();
+            ret.lquery = (text, values) => {
+                if (values) console.log('lquery:', text, values);
+                else console.log('lquery:', text);
+                return ret.query(text, values);
+            };
+
+            return ret;
+        } catch (err) {
+            console.log('Failed To Connect To Pool', err);
+            throw err;
+        }
+    }
+
+    /**
+     *  Add a search term to the table if it doesn't already exist.
+     */
+    async selectAppID() {
+        console.log('Inserting an appID into the comments DB.');
+        const client = await this.connect();
+        try {
+            const res = await client.lquery('select * from appid order by last_scraped nulls first limit 1');
+            if (res.rowCount == 1) {
+                const row = res.rows[0];
+                return {
+                    appID: row.id,
+                    idNum: row.id_num,
+                    lastScraped: row.last_scraped
+                }
+            } else {
+                throw 'Incorrect amount of Rows returned...';
+            }
+        } catch (err) {
+            console.log('Error Querying the DB.', err)
+        } finally {
+            client.release();
+        }
+        return {
+            appID: '',
+            idNum: -1,
+            lastScraped: undefined
+        }
+    }
+
+    async setScrapedDate(idNum) {
+        console.log('Updating App ID\'s last scraped date');
+        const client = await this.connect();
+        try {
+            const res = await client.lquery('update appID set last_scraped = CURRENT_TIMESTAMP where id_num = $1', [idNum]);
+        } catch (err) {
+            console.log('Error Updating last_scraped date of', appID, '\n\n', err);
+        } finally {
+            client.release();
+        }
+    }
+
+    async insertComment(appID, idNum, comment) {
+        console.log('Inserting comment into the database.');
+        console.log('App ID:', appID, '\nAuthor:', comment.author, '\nDate:', comment.date);
+        const client = await this.connect();
+        try {
+            await client.lquery(
+                'insert into comments(app_id_num, app_id, author, date_written, rating, json_object) values ($1,$2,$3,$4,$5, $6)', [idNum, appID, comment.author, comment.date, comment.rating.replace(/\D/g, ''), JSON.stringify(comment)]
+            );
+        } catch (err) {
+            console.log('Error inserting comment into the database', err);
+        } finally {
+            client.release();
+        }
+    }
+}
+
+module.exports = DB;
--- db/init_db.sql
+++ db/init_db.sql
@@ -2,6 +2,25 @@
 
 begin;
 
+
+create table reviewers(
+
+);
+
+create table review(
+    id              serial not null,
+    book_title      text   not null,
+    book_url        text   not null,
+    book_author     text   not null,
+    book_author_url text   not null,
+    review_author   text   not null,
+    review_date     date   not null,
+    review_url      text   not null,
+    book_isbn       text   not null,
+    review          text   not null,
+    rating          int64  not null
+);
+
 create table book_details(
     id              serial  not null,
     title           text    not null,
