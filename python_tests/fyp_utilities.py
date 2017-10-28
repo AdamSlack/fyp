bd1c9fd0c0d0fb65885571f8e7e0b96699440f32
Adam Slack <adamslack@outlook.com>
2017-10-28 13:22:33 +0100

    added git ignore

--- python_tests/fyp_utilities.py
+++ python_tests/fyp_utilities.py
@@ -1,13 +1,16 @@
 from time import time
 import glob
 from os import path
-import nltk
+from nlkt.tag import StanfordNERTagger
+from nltk.tokenise import word_tokenize
 
 def read_file(fp):
-    print('Opening File: ' + fp)
     if path.isfile(fp):
         with open(fp, encoding='utf-8') as f:
+            print('Opening File: ' + fp)
             return f.read()
+    else:
+        print('Provided file path does not point at a file.')
 
 def load_data_samples(dirpath, seperator='/', globule='*.txt'):
     """ Reads all txt files at a specified location and returns array of data. """
@@ -22,14 +25,32 @@ def time_action(action, *args):
     print("Action done in: %0.3fs." % (time() - start))
     return res
 
+def process_chunk_tree(t):
+        """ recursively extract entities in tagged tree """
+        # es = []
+        # if isinstance(t, tree.Tree):
+        #     if t.node == 'NE':
+        #         es.append(' '.join([ch[0] for ch in t]))
+        #     else:
+        #         for ch in t:
+        #             es.extend(process_chunk_tree(ch))
+        # print(es)
+        # return es
+    
 def extract_entities(document):
     """ """
-    print(process_document(document))
-    
-def process_document(document):
+    document_tree = process_document_pos(document)
+    entities = process_chunk_tree(document_tree)
+    for e in entities:
+        print(e)
+        
+def process_document_pos(document):
     """ tokenise Document into chunked sentences. """
-    tokenised_sentences = [nltk.word_tokenise(sent) for sent in nltk.sent_tokenize(document)]
-    return nltk.ne_chunk_sents([nltk.pos_tag(sent) for sent in tokenised_sentences])
+    # tokenised_sentences = [word_tokenize(sent) for sent in sent_tokenize(document)]
+    # return ne_chunk_sents([pos_tag(sent) for sent in tokenised_sentences])
+    tagg
+    return word_tokenize(document)
     
-nltk.download()
+
+fp = 'C:\\Users\\Adam Slack\\Documents\\GitHub\\fyp\\data\\books\\alice_in_wonderland.txt'
 extract_entities(read_file(fp))
